a21383edf7cc82c361ce2c301931e38ebedc7c69
yabsira <yabsiragirama260@gmail.com>
2024-11-12 21:01:28 +0300

    header and footer

---
+++ netflex-clone/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../../Components/Footer/footer'
+import Header from '../../Components/Header/header'
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Footer/>
+     
+  </div>
+  )
+}
+
+export default Home
