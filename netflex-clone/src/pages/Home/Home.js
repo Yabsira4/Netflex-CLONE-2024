20f375f467496a5d4e0d535094cd65eb15126371
yabsira <yabsiragirama260@gmail.com>
2024-11-13 16:52:21 +0300

    banner

--- netflex-clone/src/pages/Home/Home.js
+++ netflex-clone/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from '../../Components/Footer/footer'
 import Header from '../../Components/Header/header'
+import Banner from '../../Components/Banner/Banner'
 const Home = () => {
   return (
     <div>
       <Header />
+      <Banner/>
       <Footer/>
      
   </div>
--- netflex-clone/src/utils/requests.js
+++ netflex-clone/src/utils/requests.js
@@ -1,5 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
-//  API_KEY = 7247f3b7582cfa3c05776380696954eb
+
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
