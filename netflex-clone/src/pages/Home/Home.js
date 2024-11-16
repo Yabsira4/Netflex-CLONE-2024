bc84c1ac5ff9a6f5644ccdffd9bf8c54b0d5477f
yabsira <yabsiragirama260@gmail.com>
2024-11-16 13:22:01 +0300

    row

--- netflex-clone/src/pages/Home/Home.js
+++ netflex-clone/src/pages/Home/Home.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from '../../Components/Footer/footer'
 import Header from '../../Components/Header/header'
 import Banner from '../../Components/Banner/Banner'
+import Rowlist from '../../Components/Rows/Rowlist/Rowlist'
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner/>
+      <Rowlist/>
       <Footer/>
      
   </div>
